dd683bc5fc162d39a2f6b3c4ba1f0ba743c657c9
Tania-Bonilla11 <t.a.b.a.tania@hotmail.com>
2020-03-02 23:25:23 -0600

    Cambios

---
+++ main.js
@@ -0,0 +1,67 @@
+document.addEventListener("DOMContentLoaded", e=>{
+    const form = document.querySelector("#frmEstadistica");
+    form.addEventListener("submit", event=>{
+        event.preventDefault();
+  
+        let de = document.getElementById("selectCalculo"),
+
+        cantidad = document.querySelector("#numero").value;
+
+        let $res = document.querySelector("#lblRespuesta");
+        
+        if (de.value == "media"){
+            let lista = cantidad.split(",").map(Number);
+
+            let sum = lista.reduce((previous, current) => current += previous);
+            let avg = sum / lista.length;
+            $res.innerHTML = avg;
+        } else if (de.value=="mediana") {
+            let lista = cantidad.split(",").map(Number);
+            lista.sort((a,b)=>a-b);
+            let lowmiddle = Math.floor((lista.length-1)/2);
+            let highmiddle = Math.ceil((lista.length-1)/2);
+            let medianaa= (lista[lowmiddle]+lista[highmiddle])/2;
+            $res.innerHTML = medianaa;
+
+
+           
+
+
+        } else if (de.value=="moda") {
+            let lista = cantidad.split(",").map(Number);
+            let max=0,val=0,defmod=0;
+            var counter={};
+            var mode =[];
+            for(var i in lista){
+                if(!(lista[i] in counter))
+                counter[lista[i]]=0;
+                counter[lista[i]]++;
+                if(counter[lista[i]]==max)
+                mode.push(lista[i]);
+                else if (counter[lista[i]]>max){
+                    max = counter[lista[i]];
+                    mode = [lista[i]];
+                    $res.innerHTML = mode;
+                }
+            }
+        
+
+
+        } else if (de.value=="armonica") {
+            let lista = cantidad.split(",").map(Number);
+        
+            var media_armonica;
+           
+            let sum = lista.reduce((previous, current) => current += previous);
+             summ= 1/lista;
+             armo = lista.length/sum(sumn);
+             armo = media_armonica;
+             $res.innerHTML = media_armonica;
+           
+
+
+        };
+  
+
+    });
+  });
